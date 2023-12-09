c3d977319ae2e1936420866e9f6984e5ee856bcc
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-12-09 23:20:48 +0100

    include MessageField

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { useDebugState } from "use-named-state";
 import Form from "./components/Form/Form";
 import "./App.css";
+import { useEffect } from "react";
+import MessageField from "./components/MessageField/MessageField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({});
 
 function App() {
   const [name, setName] = useDebugState<string>("name", "");
@@ -8,10 +13,29 @@ function App() {
     "date",
     new Date().toLocaleDateString("sv")
   );
+  const [text, setText] = useDebugState<string>("text", "");
+  useEffect(() => {
+    if (name.length) {
+      const messageText = `${name} was born on ${new Date(
+        date
+      ).toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      })}.`;
+      setText(messageText);
+    } else {
+      setText("");
+    }
+  }, [date, name, setText]);
   return (
-    <div id="App">
-      <Form name={name} setName={setName} date={date} setDate={setDate} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div id="App">
+        <Form name={name} setName={setName} date={date} setDate={setDate} />
+        <MessageField text={text} />
+      </div>
+    </ThemeProvider>
   );
 }
 
